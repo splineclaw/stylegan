bfb667646f873f21316681d57cf9e08ab1799f12
splineclaw <37070764+splineclaw@users.noreply.github.com>
2019-07-18 18:56:21 -0400

    faster configuration for test purposes

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ if 0:
 #----------------------------------------------------------------------------
 # For training smaller, 128 dim datasets.
 
-if 1:
+if 0:
     desc          = 'sgan'                                                                 # Description string included in result subdir name.
     train         = EasyDict(run_func_name='training.training_loop.training_loop')         # Options for training loop.
     G             = EasyDict(func_name='training.networks_stylegan.G_style')               # Options for generator network.
@@ -124,6 +124,49 @@ if 1:
     desc += '-VERBOSE'; sched.tick_kimg_base = 1; sched.tick_kimg_dict = {}; train.image_snapshot_ticks = 1; train.network_snapshot_ticks = 100
 
  
+#----------------------------------------------------------------------------
+# For training smaller, 128 dim datasets, BUT FASTER
+
+if 1:
+    desc          = 'sgan-fast'                                                                 # Description string included in result subdir name.
+    train         = EasyDict(run_func_name='training.training_loop.training_loop')         # Options for training loop.
+    G             = EasyDict(func_name='training.networks_stylegan.G_style')               # Options for generator network.
+    D             = EasyDict(func_name='training.networks_stylegan.D_basic')               # Options for discriminator network.
+    G_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                          # Options for generator optimizer.
+    D_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                          # Options for discriminator optimizer.
+    G_loss        = EasyDict(func_name='training.loss.G_logistic_nonsaturating')           # Options for generator loss.
+    D_loss        = EasyDict(func_name='training.loss.D_logistic_simplegp', r1_gamma=10.0) # Options for discriminator loss.
+    dataset       = EasyDict()                                                             # Options for load_dataset().
+    sched         = EasyDict()                                                             # Options for TrainingSchedule.
+    grid          = EasyDict(size='1080p', layout='random')                                   # Options for setup_snapshot_image_grid().
+    metrics       = [metric_base.fid50k]                                                   # Options for MetricGroup.
+    submit_config = dnnlib.SubmitConfig()                                                  # Options for dnnlib.submit_run().
+    tf_config     = {'rnd.np_random_seed': 824}                                           # Options for tflib.init_tf().
+
+    # Dataset.
+    #desc += '-ffhq';     dataset = EasyDict(tfrecord_dir='ffhq');                 train.mirror_augment = True
+    #desc += '-ffhq512';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=512); train.mirror_augment = True
+    #desc += '-ffhq256';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=256); train.mirror_augment = True
+    #desc += '-celebahq'; dataset = EasyDict(tfrecord_dir='celebahq');             train.mirror_augment = True
+    #desc += '-bedroom';  dataset = EasyDict(tfrecord_dir='lsun-bedroom-full');    train.mirror_augment = False
+    #desc += '-car';      dataset = EasyDict(tfrecord_dir='lsun-car-512x384');     train.mirror_augment = False
+    #desc += '-cat';      dataset = EasyDict(tfrecord_dir='lsun-cat-full');        train.mirror_augment = False
+    desc += '-maw';       dataset = EasyDict(tfrecord_dir='/content/drive/My Drive/stylegan/maw-128-dataset');       train.mirror_augment = True
+
+    # Number of GPUs.
+    desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}
+    #desc += '-2gpu'; submit_config.num_gpus = 2; sched.minibatch_base = 8; sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}
+    #desc += '-4gpu'; submit_config.num_gpus = 4; sched.minibatch_base = 16; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16}
+    #desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}
+
+    # Default options.
+    train.total_kimg = 1000
+    sched.lod_initial_resolution = 4
+    sched.G_lrate_dict = {64: 0.005, 128: 0.002, 256: 0.003, 512: 0.005, 1024: 0.005}
+    sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
+
+    desc += '-VERBOSE'; sched.tick_kimg_base = 10; sched.tick_kimg_dict = {}; train.image_snapshot_ticks = 1; train.network_snapshot_ticks = 100
+
 
 #----------------------------------------------------------------------------
 # Official training configs for Progressive GAN, targeted mainly for CelebA-HQ.
